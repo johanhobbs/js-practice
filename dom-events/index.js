adb6945f65e76a0677bfba07b8fc72370c706c82
johanhobbs <johanhobbsnz@gmail.com>
2023-09-12 12:25:40 +1200

    Count h2 tags

--- dom-events/index.js
+++ dom-events/index.js
@@ -1,17 +1,22 @@
-// NOTE
-/*
-If the parenthesis after document.getElementById are empty, it will return null
-*/
-const p = document.getElementById('message')
-console.log(p);
-
-let btn = document.getElementById('btnRadio')
-
+let btn = document.getElementById('btnCount')
 btn.addEventListener('click', () => {
-    let languages = document.getElementsByName('language')
-    languages.forEach((language) => {
-        if(language.checked){
-            output.innerText = `You code with ${language.value}`
-        }
-    })
+    let hTwoHeader = document.getElementsByTagName('h2')
+    alert(`The number of h2 tags is: ${hTwoHeader.length}`)
 })
+
+
+// NOTE
+
+// const p = document.getElementById('message')
+// console.log(p);
+
+// let btn = document.getElementById('btnRadio')
+
+// btn.addEventListener('click', () => {
+//     let languages = document.getElementsByName('language')
+//     languages.forEach((language) => {
+//         if(language.checked){
+//             output.innerText = `You code with ${language.value}`
+//         }
+//     })
+// })
